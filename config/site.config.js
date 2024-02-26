2344cb0515ca0a9d76dae2e9c125fb96a8a6469b
Rokhiq <102243724+No70mi@users.noreply.github.com>
2024-02-26 07:05:44 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Anime', '/Manga', /OST],
+  protectedRoutes: ['/Anime', '/Manga', '/OST'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
